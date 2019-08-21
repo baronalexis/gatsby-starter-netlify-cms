5f7278ff82c0dbeb0b5cb4e2c813ead9355abe1e
Alexis Baron <alex.bar98@gmail.com>
2019-08-21 17:07:09 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Gatsby + Netlify CMS Starter',
     description:
       'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
-   siteUrl: `testforestry.netlify.com`
+   siteUrl: `alexisbaron.netlify.com`
 
   },
   plugins: [
